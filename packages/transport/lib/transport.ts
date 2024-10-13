e33941b3901f4aa37179e43095c5bea5459cd50e
Denis Ilchishin <ilchishin.denis@gmail.com>
2024-10-13 16:01:45 +0700

    bump neemata version

--- packages/transport/lib/transport.ts
+++ packages/transport/lib/transport.ts
@@ -1,16 +1,10 @@
-import { Hook, createPlugin } from '@nmtjs/application'
+import { createTransport } from '@nmtjs/application'
 import { HttpTransportServer } from './server.ts'
 import type { HttpTransportOptions } from './types.ts'
 
-export const HttpTransport = createPlugin<HttpTransportOptions>(
+export const HttpTransport = createTransport<HttpTransportOptions>(
   'HttpTransport',
   (app, options) => {
-    const server = new HttpTransportServer(app, options)
-    app.hooks.add(Hook.OnStartup, async () => {
-      await server.start()
-    })
-    app.hooks.add(Hook.OnShutdown, async () => {
-      await server.stop()
-    })
+    return new HttpTransportServer(app, options)
   },
 )
